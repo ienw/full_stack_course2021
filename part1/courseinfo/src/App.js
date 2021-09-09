9a7414910a58562220ab6a4b1feea1ae9a86c14d
ienw <ienw@metropolia.fi>
2021-09-09 10:41:00 +0300

    exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.courseName}</h1>
   )
 }
 
@@ -25,33 +25,34 @@ const Content = (props) => {
       <Part parts1={props.parts[1].name} parts2={props.parts[1].exercises}></Part>
       <Part parts1={props.parts[2].name} parts2={props.parts[2].exercises}></Part>
     </div>
-    
   )
 }
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div> 
-      <Header course={course} />
-      <Content parts={parts}></Content>
-      <Total parts={parts}></Total>
+      <Header courseName={course.name} />
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
     </div>  
   )
 }

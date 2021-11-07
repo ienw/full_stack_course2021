5a26abef8612c27722caaebcbe358a4ca219529e
ienw <ienw@metropolia.fi>
2021-11-07 20:15:13 +0200

    part2 exercise 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -57,6 +57,18 @@ const Notification = ({ message }) => {
   )
 }
 
+const Error = ({ error }) => {
+  if (error === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {error}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
 
@@ -67,6 +79,8 @@ const App = () => {
   const [search, setSearch] = useState('')
 
   const [notification, setNotification] = useState(null)
+
+  const [error, setError] = useState(null)
 
   React.useEffect(() => {
     const promise = personsService.getAll()
@@ -85,6 +99,10 @@ const App = () => {
       return personsService
         .remove(person.id)
         .then(() => setPersons(persons.filter((p) => p.id != person.id)))
+        .catch((e) => {
+          setError(`${person.name} can't be deleted. There's an error.`)
+          setTimeout(() => setError(null), 5000)
+        })
     }
     
   }
@@ -110,6 +128,10 @@ const App = () => {
             setNotification(`${foundPerson.name} has been updated`)
             setTimeout(() => setNotification(null), 5000)
           })
+          .catch((e) => {
+            setError(`${foundPerson.name} can't be updated. There's an error.`)
+            setTimeout(() => setError(null), 5000)
+          })
       }
     } else {
       const person = {name: newName, number: newPhone}
@@ -120,6 +142,10 @@ const App = () => {
         setPersons([...persons, response.data])
         setNotification(`Added ${person.name}`)
         setTimeout(() => setNotification(null), 5000)
+      })
+      .catch((e) => {
+        setError(`${foundPerson.name} can't be added. There's an error.`)
+        setTimeout(() => setError(null), 5000)
       })
     }
   }
@@ -140,6 +166,7 @@ const App = () => {
   return (
     <div>
       <Notification message={notification} />
+      <Error error={error}/>
       <h2>Phonebook</h2>
       <Filter search={search} handleSearchChange={handleSearchChange}/>
       <Personform 

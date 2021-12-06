690b471d08efbe9b36cc48c41c206a9229994dc1
ienw <ienw@metropolia.fi>
2021-12-06 13:28:35 +0200

    part3 ex3.12

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  const password = process.argv[2]
+  const url =
+    `mongodb+srv://lily:${password}@cluster0.tyufc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  mongoose.connect(url)
+
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+
+} else if (process.argv.length == 5) {
+
+  const password = process.argv[2]
+  const name = process.argv[3]
+  const phone = process.argv[4]
+  
+  const url =
+    `mongodb+srv://lily:${password}@cluster0.tyufc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  
+  mongoose.connect(url)
+  
+  const person = new Person({
+    name: name,
+    number: phone
+  })
+   
+  person.save().then(result => {
+    console.log(`added ${name} number ${phone} to phonebook`)
+    mongoose.connection.close()
+  })
+
+} else {
+  console.log('Please provide arguments: node mongo.js <password> <name> <phonenumber>')
+  process.exit(1)
+}

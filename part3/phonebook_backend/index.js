e93f3b9c62b72997c3a1ae88b6c4f39a6972cd83
ienw <ienw@metropolia.fi>
2021-12-06 18:38:55 +0200

    part3 ex3.16-3.18

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 const Person = require('./models/person')
+const person = require('./models/person')
 
 
 morgan.token('body', function (req, res) {
@@ -44,40 +45,58 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(result => response.json(result))
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(result => response.json(result)).catch(next)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const person = data.find(p => {
-      return p.id == request.params.id
-  })
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+
 })
 
 
-app.get('/info', (request, response) => {
+
+app.get('/info', async (request, response) => {
+  const allPeople = await Person.find({})
   var now = new Date();
-  response.send(`Phone book has info for ${data.length} people <br><br> ${now}`)
+  response.send(`Phone book has info for ${allPeople.length} people <br><br> ${now}`)
 })
 
-app.delete('/api/persons/:id', async (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+  const person = { ...request.body }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  .then(updatedPerson => {
+    if (updatedPerson) {
+      response.json(updatedPerson)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', async (request, response, next) => {
   const id = request.params.id
 
   try{
     const result = await Person.deleteOne({id:id})
     response.status(204).end()
   }catch(error){
-    response.status(400).json({ error })
+    next(error)
   }
   
 })
 
-app.post('/api/persons', async (request, response) => {
+app.post('/api/persons', async (request, response, next) => {
   const nameNnumber = { ...request.body }
   
   if (!nameNnumber.name || !nameNnumber.number) {
@@ -101,10 +120,23 @@ app.post('/api/persons', async (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(next)
 
   // data.push(person)
 })
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

304574de2b094d278a52e1eeec1de3988b0c5c3e
ienw <ienw@metropolia.fi>
2022-02-22 20:52:40 +0200

    part4 ex4.19

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -8,12 +9,26 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.post('/', async (request, response) => {
+  const token = getTokenFrom(request)
   const blog = new Blog(request.body)
 
-  const user = await User.findOne({})
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  // decode token to find out username
+  // use username to find correct user
+  // const user = await User.findOne({})
   if(user) {
     blog.user = user.id
   }

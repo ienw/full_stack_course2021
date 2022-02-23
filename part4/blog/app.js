2e603fcd4c93ec32e4ae0449ea90cdaca6040728
ienw <ienw@metropolia.fi>
2022-02-23 13:08:59 +0200

    part4 ex.4-20

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -7,11 +7,24 @@ const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
 
+const tokenExtractor = (request, response, next) => {
+  // code that extracts the token
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
 app.use(cors())
 app.use(express.json())
 
+
+app.use(tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
+
 
 module.exports = app
